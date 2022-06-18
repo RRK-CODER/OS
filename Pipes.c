d7262cb44b66fb341a2f6bd10224c266f5ec0f9a
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-06-18 22:54:26 +0530

    add this file

---
+++ Pipes.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main()
+{
+    int fd[2],n;
+    char buffer[100];
+    pid_t p;
+    p=fork();
+    if(p>0)
+    {
+        printf("Passing value to child \n");
+        write(fd[1], "hello\n",6 );
+    }
+    else
+    {
+        printf("Child received data\n");
+        n=read(fd[0], buffer, 100);
+        write(1,buffer,n);
+    }
+}

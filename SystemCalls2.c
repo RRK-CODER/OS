336f466693d488b7d5e63c161895c9ec2eaf9741
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-06-19 17:07:18 +0530

    Add this file

--- SystemCalls2.c
+++ SystemCalls2.c
@@ -9,8 +9,7 @@ int main()
     printf("/n Enter the text to write in file");
     n=read(0,buff,50);
     fd=open("f1",O_CREAT | O_RDWR, 0777);
-    write(fd,buff,n);
-    write(1,buff,n);
+     write(1,buff,n);
     close(fd);
     return 0;
 }
